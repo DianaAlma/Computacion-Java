7a9353f6b55bf55be030af225d369550d1bd6fa0
DianaAlma <diana.almazanm@outlook.com>
2021-09-30 01:52:30 -0500

    Actividad 6

---
+++ src/actividad6/Actividad6.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package actividad6;
+
+/**
+ *
+ * @author Diana
+ */
+public class Actividad6 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+   
+        Alumno alumno1 = new Alumno();
+        alumno1.imprimirAtributos();
+        alumno1.obtenerPromedio();
+        alumno1.obtenerCalificación();
+    }
+}
